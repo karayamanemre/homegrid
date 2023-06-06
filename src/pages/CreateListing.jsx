134b1364cb9e8b1df57156a8d98251ba7055d525
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-06-06 23:41:42 +0300

    upload images to firestore

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { db } from '../firebase.config';
 import { useNavigate } from 'react-router-dom';
-import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
+import { v4 as uuidv4 } from 'uuid';
 
 const CreateListing = () => {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
@@ -102,6 +110,54 @@ const CreateListing = () => {
       geolocation.lng = longitude;
       location = address;
     }
+
+    // Upload images to Firebase Storage
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const storageRef = ref(storage, `images/${uuidv4()}`);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          },
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image)),
+    ).catch((error) => {
+      setLoading(false);
+      toast.error('Images not uploaded: ' + error.message);
+      return [];
+    });
+
+    console.log(imgUrls);
 
     setLoading(false);
   };

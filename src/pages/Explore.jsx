94b9dfa091efb96a221cfff48e8ddf8b80a79fe8
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-06-07 20:50:19 +0300

    Update Explore.jsx

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import rentCategoryImage from '../assets/rentCategoryImage.jpg';
 import sellCategoryImage from '../assets/sellCategoryImage.jpg';
+import { MdExplore } from 'react-icons/md';
 
 const Explore = () => {
   return (
     <div className='m-4 mb-10'>
       <header className='flex shadow justify-between items-center mb-4 bg-white p-2 rounded-xl'>
         <p className='text-2xl font-semibold'>Explore</p>
+        <MdExplore className='text-[#2a93cb]' size={30} />
       </header>
       <main>
         <div className='flex shadow justify-between items-center p-2 bg-white rounded-xl mb-4'>

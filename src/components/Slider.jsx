c4414abb66601bca29453e2a8a78e88c71693fbe
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-06-08 20:21:20 +0300

    explore page slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import Spinner from './Spinner';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
+
+const Slider = () => {
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getListings = async () => {
+      const listingsRef = collection(db, 'listings');
+      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5));
+      const querySnapshot = await getDocs(q);
+
+      let listings = [];
+
+      querySnapshot.forEach((doc) => {
+        return listings.push({ id: doc.id, data: doc.data() });
+      });
+      setListings(listings);
+      setLoading(false);
+    };
+
+    getListings();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    listings && (
+      <>
+        <p className='font-bold'>Recommended</p>
+        <Swiper
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          className='mb-2'
+          autoplay={{ delay: 5000 }}
+        >
+          {listings.map((listing) => (
+            <SwiperSlide
+              key={listing.id}
+              onClick={() =>
+                navigate(`/category/${listing.data.type}/${listing.id}`)
+              }
+            >
+              <div
+                style={{
+                  background: `url(${listing.data.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='cursor-pointer relative w-full h-[200px] lg:h-[500px]'
+              >
+                <p className='text-white font-semibold absolute top-2 left-0 max-w-[90%] text-lg bg-gray-600 p-2 bg-opacity-70 rounded-xl'>
+                  {listing.data.name}
+                </p>
+                <p className='absolute top-14 left-0 font-semibold text-white bg-gray-600 p-2 bg-opacity-70 rounded-xl'>
+                  ${listing.data.discountedPrice ?? listing.data.regularPrice}{' '}
+                  {listing.data.type === 'rent' && '/ month'}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
+};
+
+export default Slider;
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 import rentCategoryImage from '../assets/rentCategoryImage.jpg';
 import sellCategoryImage from '../assets/sellCategoryImage.jpg';
 import { MdExplore } from 'react-icons/md';
+import Slider from '../components/Slider';
 
 const Explore = () => {
   return (
-    <div className='m-4 mb-10'>
+    <div className='m-4 mb-24'>
       <header className='flex shadow justify-between items-center mb-4 bg-white p-2 rounded-xl'>
         <p className='text-2xl font-semibold'>Explore</p>
         <MdExplore className='text-[#2a93cb]' size={30} />
       </header>
       <main>
+        <Slider />
         <div className='flex shadow justify-between items-center p-2 bg-white rounded-xl mb-4'>
           <p>Categories</p>
         </div>

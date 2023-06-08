7be40616f932e02ab308c2b0691c4607812b5113
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-06-08 17:30:32 +0300

    leaflet installed

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
+import { MapContainer, Marker, Popup } from 'react-leaflet';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
